a69ca5a6b96e0be79b80ff22aab587030e961cc9
Tausif282 <rahtausi@gmail.com>
2020-03-14 20:06:30 +0530

    This is food order website

---
+++ resources/js/script.js
@@ -0,0 +1,23 @@
+/* global $ */
+
+/* eslint-env jquery */
+
+$('document').ready(function() {
+ 
+    $('.js--features').waypoint(function (direction) {
+ 
+        if (direction === "down") {
+ 
+            $('nav').addClass('sticky');
+ 
+        } else {
+ 
+            $('nav').removeClass('sticky');
+ 
+        }
+ 
+    },{
+      offset: '60px'
+ 
+    });
+});
